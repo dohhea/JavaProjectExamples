ab062ed15ca4bfb6072c3ba039092bbbb7679365
Dongik Oh <dohhea@gmail.com>
2022-12-26 16:32:16 +0900

    Merged OthelloMain.java into OthelloGame.java

--- Othello(ver3.4)/src/OthelloGame.java
+++ Othello(ver3.4)/src/OthelloGame.java
@@ -23,6 +23,14 @@ public class OthelloGame extends JPanel {
 	private Font font;
 	private ActionListener listener;
 	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("OTHEELO");		// 제목 표시줄
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// x 박스 클릭시 화면 닫음.	
+		frame.getContentPane().add(new OthelloGame());		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 	public OthelloGame()	
 	{
 		// 초기화.		
